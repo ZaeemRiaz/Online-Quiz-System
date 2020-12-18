833828219913b7fa2990c5f64cc9d91c55e2eaef
Mohammad Mannan Akmal <mmannanakmal@gmail.com>
2020-12-18 16:01:37 +0500

    quiz feedback screen init

--- app/src/main/java/com/se/onlinequizsystem/InstructorQuizFeedbackActivity.java
+++ app/src/main/java/com/se/onlinequizsystem/InstructorQuizFeedbackActivity.java
@@ -1,10 +1,12 @@
 package com.se.onlinequizsystem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class InstructorQuizFeedbackActivity extends AppCompatActivity {
+
+    private static final String TAG = "=== InstructorQuizFeedback ===";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
