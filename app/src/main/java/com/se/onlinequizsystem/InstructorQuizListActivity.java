2035457560ecf40c37948378157a2699b494acac
Zaeem Riaz <riazzaeem0@gmail.com>
2020-12-20 20:09:09 +0500

    Updated Navigation

--- app/src/main/java/com/se/onlinequizsystem/InstructorQuizListActivity.java
+++ app/src/main/java/com/se/onlinequizsystem/InstructorQuizListActivity.java
@@ -1,9 +1,14 @@
 package com.se.onlinequizsystem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class InstructorQuizListActivity extends AppCompatActivity {
 
@@ -14,5 +19,32 @@ public class InstructorQuizListActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: init");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_quiz_list);
+    }
+
+
+    public void InstructorQuizItem1Button(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("Quiz1")
+                .setMessage("SE-E\n07/12/2020 5:00PM")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton("Show Feedback", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Context context = getBaseContext();
+                        Intent intent = new Intent(context, InstructorQuizFeedbackActivity.class);
+                        startActivity(intent);
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton("Grade Quiz", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Context context = getBaseContext();
+                        Intent intent = new Intent(context, InstructorQuizGradeActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .show();
     }
 }
