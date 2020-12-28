a75ce94a0796523aa269313cae49001f172e55bb
Zaeem Riaz <riazzaeem0@gmail.com>
2020-12-28 22:58:57 +0500

    Added data in db

--- app/src/main/java/com/se/onlinequizsystem/LoginActivity.java
+++ app/src/main/java/com/se/onlinequizsystem/LoginActivity.java
@@ -60,7 +60,7 @@ public class LoginActivity extends AppCompatActivity {
             prep.bindLong(4, 0);
             prep.execute();
             long rowId = prep.executeInsert();
-            Log.d(TAG, "bogusEntries: first statement");
+            Log.d(TAG, "bogusEntries: statement 1");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -73,7 +73,7 @@ public class LoginActivity extends AppCompatActivity {
             prep.bindString(3, "pass");
             prep.bindLong(4, 1);
             prep.execute();
-            Log.d(TAG, "bogusEntries: second statement");
+            Log.d(TAG, "bogusEntries: statement 2");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -82,7 +82,7 @@ public class LoginActivity extends AppCompatActivity {
             String sql = "insert into quiz (quizID, quizName, difficulty, openTime, closeTime, totalTimeInSeconds, totalQuestions, totalMarks, instructions)" +
                     "values (1, 'sample quiz 1', 3, 1609030194, 1609030194, 3600, 3, 3, 'Attempt all questions')";
             db.execSQL(sql);
-            Log.d(TAG, "bogusEntries: third statement");
+            Log.d(TAG, "bogusEntries: statement 3");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -90,7 +90,7 @@ public class LoginActivity extends AppCompatActivity {
         try {
             String sql = "insert into quizQuestions(quizID, questionID)" + "values (1, 1)";
             db.execSQL(sql);
-            Log.d(TAG, "bogusEntries: fourth statement");
+            Log.d(TAG, "bogusEntries: statement 4");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -99,7 +99,61 @@ public class LoginActivity extends AppCompatActivity {
             String sql = "insert into question (qID, qType, qText, qMarks, posAns1, posAns2, posAns3, posAns4, valAns1, valAns2, valAns3, valAns4) "
                     + "values (1, 1, 'Which signs show that a plant has been grown in soil deficient in magnesium ions', 10, 'no flowers and poor root growth','small leaves and ore roots','white upper leaves and no flowers','yellow stem and yellow leaves',0,1,0,0)";
             db.execSQL(sql);
-            Log.d(TAG, "bogusEntries: fifth statement");
+            Log.d(TAG, "bogusEntries: statement 5");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            String sql = "insert into quiz (quizID, quizName, difficulty, openTime, closeTime, totalTimeInSeconds, totalQuestions, totalMarks, instructions)" +
+                    "values (2, 'sample quiz 2', 2, 1609030194, 1609030194, 1200, 1, 1, 'Attempt all questions');";
+            db.execSQL(sql);
+            Log.d(TAG, "bogusEntries: statement 6");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            String sql = "insert into quiz (quizID, quizName, difficulty, openTime, closeTime, totalTimeInSeconds, totalQuestions, totalMarks, instructions)" +
+                    "values (3, 'Plants', 2, 1609030194, 1609030194, 1200, 1, 1, 'Attempt all questions');";
+            db.execSQL(sql);
+            Log.d(TAG, "bogusEntries: statement 7");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            String sql = "insert into question (qID, qType, qText, qMarks, posAns1, posAns2, posAns3, posAns4, valAns1, valAns2, valAns3, valAns4) "
+                    + "values (2, 1, 'How many legs does a spider have', 10, '1','2','3','8',0,0,0,1);";
+            db.execSQL(sql);
+            Log.d(TAG, "bogusEntries: statement 8");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            String sql = "insert into question (qID, qType, qText, qMarks, posAns1, posAns2, posAns3, posAns4, valAns1, valAns2, valAns3, valAns4) "
+                    + "values (3, 1, 'How many legs does a human have', 10, '1','2','3','8',0,1,0,0);";
+            db.execSQL(sql);
+            Log.d(TAG, "bogusEntries: statement 9");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            String sql = "insert into question (qID, qType, qText, qMarks, posAns1, posAns2, posAns3, posAns4, valAns1, valAns2, valAns3, valAns4) "
+                    + "values (4, 1, 'What is the square root of 36', 10, '2','3','5','6 leaves',0,0,0,1);";
+            db.execSQL(sql);
+            Log.d(TAG, "bogusEntries: statement 10");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            String sql = "insert into quizQuestions(quizID, questionID)" + "values (2, 3), (3, 2), (1, 4), (1, 3)";
+            db.execSQL(sql);
+            Log.d(TAG, "bogusEntries: statement 11");
 
         } catch (Exception e) {
             e.printStackTrace();
